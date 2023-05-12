b6f50c28aefb1f4dda61d8c4df7d247d8e87fea3
Jpl08 <132668668+Jpl08@users.noreply.github.com>
2023-05-12 14:35:49 +0530

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -219,11 +219,11 @@ let m2 = `
 ▢ $
 └───────────  
 
-Bot has more than 300 commands and i am lazy to write all
+Bot has more than 300 commands to write all
 `
 //const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     let pp = './Guru.jpg' 
-    conn.sendButton(m.chat, m2, '▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n', pp, [
+    conn.sendButton(m.chat, m2, '▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Follow on Instagram\n\n', pp, [
       ['⏍ Info', `${usedPrefix}botinfo`],
       ['⌬ Groups', `${usedPrefix}gpguru`]
     ],m, rpyt)
